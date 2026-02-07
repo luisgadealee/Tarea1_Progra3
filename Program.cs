edf5799380aa3028f69899894a13f18ff2fd5785
Luis Gadea Lee <luisfergadea1298@gmail.com>
2026-02-07 11:36:50 -0600

    correccion de main

--- Program.cs
+++ Program.cs
@@ -14,23 +14,7 @@ namespace Sistema_Gestion_Bancaria
             System.Console.WriteLine("Prueba");
             System.Console.WriteLine("Prueba Luis para subir cambio");
             System.Console.WriteLine("Prueba Jorge .2");
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-
-=======
             System.Console.WriteLine("Prueba Luis para subir cambio 2");
->>>>>>> 149317e70625cb19975738dcc124d9920304a43f
             System.Console.WriteLine("Prueba Jorge 3 - Linea simultanea mientras edita su codigo.");
         }
     }
