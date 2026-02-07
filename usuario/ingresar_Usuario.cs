5c9a666fe8c76ac6c23db637c10ec30acca80a43
Luis Gadea Lee <luisfergadea1298@gmail.com>
2026-02-07 11:33:12 -0600

    creacion de folder usuario y clase para ingresar usuario

---
+++ usuario/ingresar_Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gestion_Bancaria.usuario
+{
+    internal class ingresar_Usuario
+    {
+    }
+}
