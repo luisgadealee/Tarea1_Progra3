df431c6d7f5e9ae9c9812ed113f3ccae71855261
Jorge_A <jorgeman10@gmail.com>
2026-02-07 17:58:26 -0600

    Corrigiendo folder Interface

--- Interface/Class1.cs
+++ Interface/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sistema_Gestion_Bancaria
+namespace Sistema_Gestion_Bancaria.Interface
 {
-    internal class IPrestamo
+    internal class Class1
     {
-
     }
 }
