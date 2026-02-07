4d829363aff9dca620b7ec495fbd428ad8ae22bb
Luis Gadea Lee <luis.gadea@uamcr.net>
2026-02-07 10:44:57 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gestion_Bancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Bienvenido al Sistema de Gestión Bancaria");
+        }
+    }
+}
