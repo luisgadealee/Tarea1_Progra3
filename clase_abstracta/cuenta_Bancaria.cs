56cedbd1b23bc60ee1910fbcc5e0b114ea47422d
Luis Gadea Lee <luisfergadea1298@gmail.com>
2026-02-08 11:07:16 -0600

    clase asbtratcta

---
+++ clase_abstracta/cuenta_Bancaria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gestion_Bancaria.clase_abstracta
+{
+    abstract public class cuenta_Bancaria
+    {
+        public string numeroCuenta { get; set; }
+        public string titular { get; set; }
+        public decimal saldo { get; set; }
+        public cuenta_Bancaria(string numeroCuenta, string titular, decimal saldo)
+        {
+            this.numeroCuenta = numeroCuenta;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+        public abstract void Depositar(decimal monto);
+        public abstract void Retirar(decimal monto);
+    }
+}
